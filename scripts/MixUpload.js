1dccd7082b10b52c75d576966942a8b52d7c05bf
Nickolay Shapovalov <zniki.ru@gmail.com>
2012-11-13 11:53:41 +0400

    initial commit

---
+++ scripts/MixUpload.js
@@ -0,0 +1,96 @@
+$(function() {
+  /* Обработчик события OnScroll. Собственно здесь и происходит основная работа */
+  var onPageChangedHandler = function ()
+  {
+    var nodes = getAudioNodes();
+    allDownloadLinksModify(nodes);
+
+    setTimeout(onPageChangedHandler, 1000);
+  };
+
+  /* Получить список всех аудио-узлов */
+  function getAudioNodes()
+  {
+    return $('div[id^="pl_track"]');
+  };
+
+  /* Делаем рабочую ссылку на скачивание  */
+  function allDownloadLinksModify(nodes)
+  {
+    for (var i = 0; i < nodes.length; i++)
+    {
+      getTrackId(nodes[i]);
+      var link = $(nodes[i]).find('div.menudl a:first');
+      if ($(link).attr('onclick')) {
+        $(link).removeAttr('onclick');
+        linkModify(link, nodes[i]);
+      }
+    }
+  };
+
+  /* Получить название трека */
+  function getTrackTitle(node)
+  {
+    if (typeof node != 'undefined') {
+      //return $.trim($(node).find('a.track-title').text().split(' ').join('_')) + '.mp3';
+      return $.trim($(node).find('a.track-title').text()) + '.mp3';
+    }
+    else {
+      //return $.trim($("a#p_tracktitle").text().split(' ').join('_')) + '.mp3';
+      return $.trim($("a#p_tracktitle").text()) + '.mp3';
+    }
+  };
+  
+  /* Получить ссылку на трек */
+  function getDownloadLink(node)
+  {
+    var id = getTrackId(node);
+    var url = 'http://p0.mixupload.org/player/play/' + id + '/0/track.mp3';
+    return url;
+  };
+  
+  /* Получить номер (id) трека */
+  function getTrackId(node)
+  {
+    var pattern = /[0-9]+$/;
+    if ( typeof node != 'undefined' ) {
+      /* треки есть на странице */
+      return parseInt( pattern.exec(node.id)[0] );
+    } 
+    else {
+      /* на странице уже нет песен */
+      return parseInt( pattern.exec($('a#p_download').attr('href')) );
+    }
+  };
+  
+  /* Изменить ссылку для скачивания. 
+    a - html object link to modify
+    node - 
+  */
+  function linkModify(a, node){
+    $(a).attr('download', getTrackTitle(node)  );
+    $(a).attr('href'    , getDownloadLink(node));
+    $(a).attr('title'   , 'Download now!');
+    $(a).removeAttr('target');
+  }
+  
+  /* Кнопка "СКАЧАТЬ" рядом с плеером */
+  function initDownloadPlayerButton()
+  {
+    var pattern = /[0-9]+$/;
+    var trackId = pattern.exec($(this).attr('href'));
+    if (trackId != null) {
+      var node = document.getElementsByClassName('track-now')[0];
+      linkModify(this, node);
+    }
+  };
+
+  function initialize()
+  {
+    /* Кнопка скачать рядом с плеером */
+    $('a#p_download').mouseover( initDownloadPlayerButton );
+    onPageChangedHandler();
+    $('a#regLink').removeAttr('id');
+  };
+  initialize();
+});
---
+++ scripts/background.js
@@ -0,0 +1,21 @@
+chrome.webRequest.onHeadersReceived.addListener(
+  /* Удаляем имя файла из полученных хедеров,
+     для работы аттрибута a[download] 
+  */
+  function(obj) {
+    var newHeaders = obj.responseHeaders;
+    for (var i=0; i<newHeaders.length; i++) {
+      if (newHeaders[i].name == "Content-Disposition")
+          newHeaders[i].value = "attachment";
+    }
+    return {responseHeaders: newHeaders};
+  },
+  // filters
+  {
+    urls: [
+      'http://*.mixupload.org/*.mp3'
+    ]
+  },
+  // extraInfoSpec
+  ['blocking', 'responseHeaders']
+);
